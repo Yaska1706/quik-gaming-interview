17ff9ae3b3d786a0c46f0b4b31f8e7088ecc40a9
Yasin Kinyua Murithi <yasinkinyua@gmail.com>
2022-04-01 09:49:52 +0300

    Feature: Setup app executable
    Signed-off-by: Yasin Kinyua Murithi <yasinkinyua@gmail.com>

---
+++ cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/yaska1706/quik-gaming-interview/pkg/api"
+	"github.com/yaska1706/quik-gaming-interview/pkg/app"
+	"github.com/yaska1706/quik-gaming-interview/pkg/repository"
+)
+
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "this is the startup error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+
+	db, err := repository.SetupDB()
+	if err != nil {
+		return err
+	}
+
+	storage := repository.NewStorage(db)
+
+	// create router dependency
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	walletService := api.NewWalletService(storage)
+	server := app.NewServer(router, walletService)
+
+	// start the server
+	if err := server.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
